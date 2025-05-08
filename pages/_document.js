e7596d86de017fe3166d572a23b1c0a0049fb2ee
Enteirinho <106485582+Enteirinho@users.noreply.github.com>
2025-05-07 21:12:13 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* ✅ Estilos permitidos aqui */}
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
           rel="stylesheet"
